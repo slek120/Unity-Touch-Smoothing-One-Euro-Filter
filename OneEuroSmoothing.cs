d02cde0793881cf6c7d3d11c91d6da499da10c64
Eric Mascot <eric.mascot@donuts.ne.jp>
2014-01-07 15:14:40 +0700

    Initial Commit

--- OneEuroSmoothing.cs
+++ OneEuroSmoothing.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class Touch2 : MonoBehaviour
+public class OneEuroSmoothing : MonoBehaviour
 {
 	//Higher values reduce jitter (fcmin or minimum cutoff frequency)
 	//Keep above 0. Start at 1. Increase until no jitter.
@@ -9,7 +9,7 @@ public class Touch2 : MonoBehaviour
 	//Higher values reduce lag (beta or slope of velocity for cutoff frequency)
 	//Keep above 0. Start at 0. Increase until no lag.
 	public float lagReduction;
-
+	
 	private Vector2 currentPosition;
 	private Vector2 currentVelocity;
 	private Vector2 filteredPosition;
@@ -36,28 +36,35 @@ public class Touch2 : MonoBehaviour
 				currentVelocity = Vector2.zero;	
 			}
 		}
-
+		
+		OneEuroFilter (currentPosition, currentVelocity, Time.deltaTime);
+		
+		transform.position = Camera.main.ScreenToWorldPoint (new Vector3 (filteredPosition.x, filteredPosition.y, 8));
+	}
+	
+	void OneEuroFilter (Vector2 currentPosition, Vector2 currentVelocity, float dt)
+	{
 		//Get a smooth velocity using exponential smoothing
-		filteredVelocity = Filter (currentVelocity, filteredVelocity, Alpha (Vector2.one, Time.deltaTime));
+		filteredVelocity = Filter (currentVelocity, filteredVelocity, Alpha (Vector2.one, dt));
 		//Use velocity to get smoothing factor for position
 		Vector2 cutoffFrequency;
 		cutoffFrequency.x = jitterReduction + lagReduction * Mathf.Abs (filteredVelocity.x);
 		cutoffFrequency.y = jitterReduction + lagReduction * Mathf.Abs (filteredVelocity.y);
-		//Get a smooth position using exponential smoothing with changing smoothing factor
-		filteredPosition = Filter (currentPosition, filteredPosition, Alpha (cutoffFrequency, Time.deltaTime));
-
-		transform.position = Camera.main.ScreenToWorldPoint (new Vector3 (filteredPosition.x, filteredPosition.y, 8));
+		//Get a smooth position using exponential smoothing with smoothing factor from velocity
+		filteredPosition = Filter (currentPosition, filteredPosition, Alpha (cutoffFrequency, dt));
 	}
-
+	
 	Vector2 Alpha (Vector2 cutoff, float dt)
 	{
-		float x = 1 / (1 + 1 / (2 * Mathf.PI * cutoff.x) / dt);
-		float y = 1 / (1 + 1 / (2 * Mathf.PI * cutoff.y) / dt);
-		x = Mathf.Clamp (x, 0, 1);
-		y = Mathf.Clamp (y, 0, 1);
-		return new Vector2 (x, y);
+		float tauX = 1 / (2 * Mathf.PI * cutoff.x);
+		float tauY = 1 / (2 * Mathf.PI * cutoff.y);
+		float alphaX = 1 / (1 + tauX / dt);
+		float alphaY = 1 / (1 + tauY / dt);
+		alphaX = Mathf.Clamp (alphaX, 0, 1);
+		alphaY = Mathf.Clamp (alphaY, 0, 1);
+		return new Vector2 (alphaX, alphaY);
 	}
-
+	
 	Vector2 Filter (Vector2 current, Vector2 previous, Vector2 alpha)
 	{
 		float x = alpha.x * current.x + (1 - alpha.x) * previous.x;
